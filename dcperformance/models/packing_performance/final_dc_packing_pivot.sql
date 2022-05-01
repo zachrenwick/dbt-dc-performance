ed2c98f2eef5e38d50edc6091ed88ed8eb945c25
Zach Renwick <zach.renwick@gmail.com>
2022-05-01 04:43:39 +0000

    cleaning up pivot

--- dcperformance/models/packing_performance/final_dc_packing_pivot.sql
+++ dcperformance/models/packing_performance/final_dc_packing_pivot.sql
@@ -1,16 +1,14 @@
 
 {{ config(materialized='table') }}
 
-{%- set my_list = adapter.get_columns_in_relation(ref('int_transactions_pivot')) -%}
+{% set column_names = dbt_utils.get_filtered_columns_in_relation(from=ref('int_transactions_pivot'), except=["order_number", "order_line", "item_number"]) %}
 
 select
 order_number,
 order_line,
 item_number
-    {% for item in my_list %}
-    , max( {{ item.name }} ) as {{ item.name }}_max_ts
-    {% if not loop.last %}    
-    {% endif %}
+    {% for column_name in column_names %}
+    , max( {{ column_name }} ) as {{ column_name }}_max
     {% endfor %}
 from {{ ref('int_transactions_pivot') }}
 group by order_number, order_line, item_number
